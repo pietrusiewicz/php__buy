f552e44fa8615646c1134e3ac2800e798a7a7d2f
programowanie <p4zdaninho.cr@gmail.com>
2022-09-14 12:13:19 +0200

    add functionality add item to todolist what contains in session variable

--- src/resources/views/profile.blade.php
+++ src/resources/views/profile.blade.php
@@ -5,6 +5,17 @@
 </head>
 <body>
     <h1>username: {{ $user }} <a href="/profile/logout/">logout</a></h1>
+    <h2>Todolist</h2>
+    <ol>
+        @foreach($items as $item)
+        <li>{{ $item }}</li>
+        @endforeach
+        <form method="post" action="add_item/">
+            @csrf
+            <li><input type="text" name="item_name"/>
+            <input type="submit" value="+"/></li>
+        </form>
+    </ol>
     
 </body>
 </html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -16,33 +16,77 @@ Route::get('/login', function (Request $request) {
     return view('login', ['info'=>""]);
 });
 
+/*
+class Todolist {
+    public static function addItem(Request $request, $item_name) {
+        $request->session()->get('todolist');//array_push($todolist,$item_name);
+        return $todolist;
+    }
+
+}
+ */
+class Profile {
+    public static function get_data(Request $request) {
+        $items = $request->session()->get('todolist');
+        return ['user'=>$request->session()->get("usname"), "items"=>$items];
+    }
+    public static function go_view(Request $request) {
+
+        return view('profile', self::get_data($request));
+    
+    }
+}
+
+Route::post('/add_item', function (Request $request) {
+    /*
+    if (Input::get('item_name')) {
+        $arr = $request->session()->get('todolist');
+        array_push($arr, $_POST['item_name']);
+        */
+    $item = $_POST['item_name'];
+    $r = $request->session();
+    #TODO
+    $r->push('todolist', $item);
+    //return Profile::go_view($request);
+    return Redirect::back();
+});
 
 Route::get('/profile', function (Request $request) {
     if ($request->session()->missing('usname')) {
         return Redirect::to('/login');
     } else {
-        return view('profile', ['user'=>$request->session()->get("usname")]);
+        #return view('profile', ['user'=>$request->session()->get("usname")]);
+        return Profile::go_view($request);
     }
 });
+
 Route::post('/profile', function (Request $request) {
-    //Profile::login($request);
-    //dd($request);
-    $usname = $_POST['usname'];
-    $passwd = $_POST['passwd'];
-    //if (isset(['usname']))
-    if (User::authenticate($usname, $passwd)) {
-        $request->session()->put("usname", "$usname");
-        return view('profile', ['user'=>$request->session()->get("usname")]);
+    if ($request->session()->missing('users')) {
+        //Profile::login($request);
+        //dd($request);
+        $usname = $_POST['usname'];
+        $passwd = $_POST['passwd'];
+        //if (isset(['usname']))
+        if (User::authenticate($usname, $passwd)) {
+            $request->session()->put("usname", "$usname");
+            $request->session()->put("todolist", ["php", "laravel"]);
+            return Profile::go_view($request);
+            #return view('profile', ['user'=>$request->session()->get("usname")]);
+        } else {
+            return view("login", ['info'=>"Invalid data"]);
+            //return View::make('login', array('info' => 'Invalid data'));
+        }
     } else {
-        return view("login", ['info'=>"Invalid data"]);
-        //return View::make('login', array('info' => 'Invalid data'));
+        return Profile::go_view($request);
+        #return view('profile', ['user'=>$request->session()->get("usname")]);
     }
 });
 
 
 Route::get('/profile/logout', function (Request $request) {
     $request->session()->forget('usname');
-    return view('login', ["info"=>'']);
+    $request->session()->forget('todolist');
+    return Redirect::to('/login');
 });
 
 Route::get('/user/{name?}', function ($name=null) {
