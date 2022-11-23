32620ed57a25d68a24fdf74fed90a457ee05f6ae
pazdaninho <p4zdaninho.cr@gmail.com>
2022-11-23 19:55:17 +0100

    exporting modules to small items

--- src/routes/web.php
+++ src/routes/web.php
@@ -110,36 +110,7 @@ Route::get('/item_list', function (Request $request) {
 });
 
 /* finance tracker */
-Route::get('/finance_tracker', function (Request $request) {
-    #TODO
-    return User::go_view($request, 'finance_tracker');
-});
-
-Route::post('/finance_tracker/append_bought_thing', function (Request $request) {
-    $item = $_POST['item_name'];
-    $price = $_POST['price'];
-    $request->session()->push('bought_things', [$item,$price]);
-    return Redirect::back();
-});
-
-Route::get('/finance_tracker/edit', function (Request $request) {
-    #TODO
-    return User::go_view($request, 'finance_tracker_editor');
-});
-
-Route::get('/finance_tracker/delete/{i?}', function (Request $request, $i=null) {
-    if ($i==null) {
-        return Redirect::back();
-    } else {
-    $items = $request->get('bought_things');
-    unset($items[$i]);
-    $request->session()->forget('bought_things');
-    $request->session()->put("bought_things", $items);
-    //$request->session()->push('bought_things', [$item,$price]);
-    return Redirect::back();
-    }
-});
-
+include "finance_tracker/finance_tracker.php";
 
 Route::get('/logout', function (Request $request) {
     $request->session()->forget('usname');
