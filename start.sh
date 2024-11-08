7fc3cb4ddd4a5dfc81084f1658f1ce3449b8069c
Kacper Pietrusiewicz <p4zdaninho.cr@gmail.com>
2024-11-08 13:02:24 +0100

    poprawa pliku start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-mv src temp && mkdir src && cd src && docker compose run --rm composer create-project laravel/laravel . && cd .. && cp -R -f temp/* src/ && rm -r temp
+mv src temp && mkdir src && cd src && docker compose run --rm composer create-project laravel/laravel . && docker compose run --rm composer require laravel/sanctum && cd .. && cp -R -f temp/* src/ && rm -r temp
